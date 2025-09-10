c5d9c96745436dbf383f5806fbd99262c9fa53d6
Rob Titsworth <rob.titsworth1106@gmail.com>
2025-09-10 00:07:53 -0600

    Bump cmd_swap formula to v0.1.1

--- Formula/cmd_swap.rb
+++ Formula/cmd_swap.rb
@@ -1,7 +1,7 @@
 class CmdSwap < Formula
   desc "Swap selected text with clipboard contents (macOS)"
   homepage "https://github.com/titsworthrob/cmd_swap"
-  url "https://github.com/titsworthrob/cmd_swap/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/titsworthrob/cmd_swap/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "c3c60e6aaf511a9afd2e50b9b5cfd1b99599ddc0194f38da68463d6619b05749"
   license "MIT"
 
