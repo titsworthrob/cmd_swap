952d34a4d8cba1c2c74db2f040e545042512d898
Rob Titsworth <rob.titsworth1106@gmail.com>
2025-09-09 23:16:18 -0600

    Fix sha256 for v0.1.0 tarball

--- Formula/cmd_swap.rb
+++ Formula/cmd_swap.rb
@@ -2,7 +2,7 @@ class CmdSwap < Formula
   desc "Swap selected text with clipboard contents (macOS)"
   homepage "https://github.com/titsworthrob/cmd_swap"
   url "https://github.com/titsworthrob/cmd_swap/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "48d90aee30f5e806cc563aa920502a239c7062e1a7e07e6beb5b987592096a21"
   license "MIT"
 
   depends_on :macos
